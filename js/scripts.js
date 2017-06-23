c69d4755236728d0b42ae1891784eb6f7fa334fa
Shradha Pulla <pullashradha@gmail.com>
2017-06-23 13:14:47 -0700

    Created a horizontal shift method, BUT doesn't correctly replace the characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
   $("form#textInput").submit(function(event) {
     event.preventDefault();
+
+//Without RegEx
+    var row1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var row2 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+    var row3 = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
+    var row4 = ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+
     var inputtedText = $("input#textToChange").val();
     var outputtedText = [];
     for (var i = 0; i < inputtedText.length; i ++)
     {
-      outputtedText.push(inputtedText[i]);
+      if ($.inArray(inputtedText[i], row1) != -1 || $.inArray(inputtedText[i], row2) != -1 || $.inArray(inputtedText[i], row3) != -1 || $.inArray(inputtedText[i], row4) != -1) {   //inArray returns -1 if value is not in array
+        outputtedText.push(inputtedText[(inputtedText.length - 1) - i]);
+      } else {
+        outputtedText.push(inputtedText[i]);
+      }
     }
-    alert(outputtedText);
 
     $("#outputText").text(outputtedText);
   });
