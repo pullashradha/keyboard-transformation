32c6e538927099a17767b1ee13a2eeadfe140b7a
Shradha Pulla <pullashradha@gmail.com>
2017-06-23 12:39:03 -0700

    Created form to input text from index.html, and to ouput simple character split to display back on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("form#textInput").submit(function(event) {
+    event.preventDefault();
+    var inputtedText = $("input#textToChange").val();
+    var outputtedText = [];
+    for (var i = 0; i < inputtedText.length; i ++)
+    {
+      outputtedText.push(inputtedText[i]);
+    }
+    alert(outputtedText);
+
+    $("#outputText").text(outputtedText);
+  });
 });
