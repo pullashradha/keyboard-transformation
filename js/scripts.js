1bf23d3898833875dd380b0831cf3fc5d6e76aaa
Shradha Pulla <pullashradha@gmail.com>
2017-06-24 10:50:04 -0700

    Created a new array linearShiftRows for linear shift functionality, PROBLEM restarting the array count when linear shift value > 39 or shifting backwards after 0th place in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-  $("form#textInput").submit(function(event) {
+  $("form#text-form").submit(function(event) {
     event.preventDefault();
 
-//Without RegEx
+//Create keyboard row arrays
     var row1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     var row2 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
     var row3 = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
     var row4 = ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+    var linearShiftRows = row1.concat(row2).concat(row3).concat(row4); //Combine all rows for linear shift function
 
-    var inputtedText = $("textarea#textToChange").val().split('');
+//Input initial text & linear shift value
+    var inputtedText = $("textarea#text-to-change").val().split(''); //Split text input by character
+    var linearShiftValue = parseInt($("input#linear-shift").val()); //Input value for linear shift
     var outputtedText = [];
 
 //Horizontal Flip
@@ -34,25 +37,34 @@ $(document).ready(function() {
     // }
 
 //Vertical Flip
+  // for (var i = 0; i < inputtedText.length; i ++)
+  // {
+  //   if ($.inArray(inputtedText[i], row1) != -1) {
+  //     var charactersPlaceInArray = row1.indexOf(inputtedText[i], row1);
+  //     outputtedText.push(row4[charactersPlaceInArray]);
+  //   } else if ($.inArray(inputtedText[i], row2) != -1) {
+  //     var charactersPlaceInArray = row2.indexOf(inputtedText[i], row2);
+  //     outputtedText.push(row3[charactersPlaceInArray]);
+  //   } else if ($.inArray(inputtedText[i], row3) != -1) {
+  //     var charactersPlaceInArray = row3.indexOf(inputtedText[i], row3);
+  //     outputtedText.push(row2[charactersPlaceInArray]);
+  //   } else if ($.inArray(inputtedText[i], row4) != -1) {
+  //     var charactersPlaceInArray = row4.indexOf(inputtedText[i], row4);
+  //     outputtedText.push(row1[charactersPlaceInArray]);
+  //   } else {
+  //     outputtedText.push(inputtedText[i]);
+  //   }
+  // }
+
+//Linear Shift
   for (var i = 0; i < inputtedText.length; i ++)
   {
-    if ($.inArray(inputtedText[i], row1) != -1) {
-      var charactersPlaceInArray = row1.indexOf(inputtedText[i], row1);
-      outputtedText.push(row4[charactersPlaceInArray]);
-    } else if ($.inArray(inputtedText[i], row2) != -1) {
-      var charactersPlaceInArray = row2.indexOf(inputtedText[i], row2);
-      outputtedText.push(row3[charactersPlaceInArray]);
-    } else if ($.inArray(inputtedText[i], row3) != -1) {
-      var charactersPlaceInArray = row3.indexOf(inputtedText[i], row3);
-      outputtedText.push(row2[charactersPlaceInArray]);
-    } else if ($.inArray(inputtedText[i], row4) != -1) {
-      var charactersPlaceInArray = row4.indexOf(inputtedText[i], row4);
-      outputtedText.push(row1[charactersPlaceInArray]);
-    } else {
-      outputtedText.push(inputtedText[i]);
+    if ($.inArray(inputtedText[i], linearShiftRows) != -1) {
+      var charactersPlaceInArray = linearShiftRows.indexOf(inputtedText[i], linearShiftRows);
+      outputtedText.push(linearShiftRows[charactersPlaceInArray + linearShiftValue]);
     }
   }
 
-    $("#outputText").text(outputtedText);
+    $("#output-text").text(outputtedText);
   });
 });
